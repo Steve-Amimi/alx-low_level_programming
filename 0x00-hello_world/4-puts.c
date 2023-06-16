6fae38e36896c98f107f0ee72ccaf19eb81991b4
Stephen Amimi <stephenamimi@gmail.com>
2023-06-15 21:39:02 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints a line with puts fuction
+ * Return: 0
+ */
+int main(void)
+{
+puts{"with proper grammar, but the outcome is a piece of art,"};
+return (0);
+}

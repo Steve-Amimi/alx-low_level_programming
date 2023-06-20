722e61255edf568cd1c542b93d4977308ea75910
Stephen Amimi <stephenamimi@gmail.com>
2023-06-19 21:37:07 -0700

    6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Begin
+ * Return: 0 (Success)
+ */
+int main(void){
+	printf(sizeof(char));
+	printf(sizeof(int));
+	printf(sizeof(long int));
+	printf(sizeof(long long int));
+	printf(sizeof(float));
+	return (0);
+}

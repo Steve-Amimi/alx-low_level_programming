4fac8f45828b0e2669af2129a1baabdd3f0bb744
Stephen Amimi <stephenamimi@gmail.com>
2023-06-19 21:26:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - program that prints a line with puts fuction
- * Return: 0
+ * Return: 0 (Succees)
  */
 int main(void)
 {
-puts{"\"Programming is like building a multilingual puzzle"};
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

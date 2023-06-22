aa370c9ea1d174f91bec9fa275e2c53d7ae7d4da
Stephen Amimi <stephenamimi@gmail.com>
2023-06-22 00:15:33 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,8 +24,7 @@ int main(void)
 	}
 	else
 	{
-                printf("Last digit of %d, is %d and is 0\n", n, n%d);
-        }
-	
-return (0);
+		printf("Last digit of %d, is %d and is 0\n", n, n%d);
+	}
+	return (0);
 }

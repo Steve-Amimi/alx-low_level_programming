af0384ad5ac70ad233c7a0c469d2c1ce651595ae
Stephen Amimi <stephenamimi@gmail.com>
2023-06-24 08:38:35 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * This program prints all numbers of base 10single
+ *
+ * Return: (0) Success
+ **/
+int main(void)
+{
+	int i
+
+	for (i = 0; i <= 9; i++)
+
+	{
+		printf("%d\n", i);
+	}
+	return (0);

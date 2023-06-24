41394cee3055f0fbf306e13b709ad019f003cbae
Stephen Amimi <stephenamimi@gmail.com>
2023-06-24 07:50:18 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * This program writes out the the alphabet in lowercase,except q and e.
+ * Return: (0) success
+ **/
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower != 'q' && lower != 'e' && lower <= 'z'; lower++)
+	{
+		putchar(lower);
+	}
+	putchar('\n');
+	return (0);
+}

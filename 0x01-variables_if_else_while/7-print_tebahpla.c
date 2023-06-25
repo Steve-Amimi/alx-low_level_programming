2fad22212f0d9d632b5a75786b870712f665a116
Stephen Amimi <stephenamimi@gmail.com>
2023-06-24 21:43:46 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * THis program prints out letters in reverse
+ *
+ * Return: (0) Success
+ **/
+int main(void)
+{
+	int chac;
+
+	for (chac = 'z'; chac >= 'a'; chac--)
+	{
+		putchar(chac);
+	}
+
+	putchar('\n');
+	return (0);

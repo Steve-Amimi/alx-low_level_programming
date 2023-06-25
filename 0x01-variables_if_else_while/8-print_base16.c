a0863ca01a44f5d92031140610a8308e27688c6c
Stephen Amimi <stephenamimi@gmail.com>
2023-06-24 21:59:21 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * THis program prints out letters in reverse
+ *
+ * Return: (0) Success
+ **/
+int main(void)
+{
+	int letter;
+	int numb;
+
+	for (numb = 0; numb <= 9; numb++)
+	{
+		putchar('0' + numb);
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n';)
+	return (0);
+}

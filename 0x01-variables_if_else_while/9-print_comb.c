7a717524fff2ab19f253e00b87df11862ad3168b
Stephen Amimi <stephenamimi@gmail.com>
2023-06-24 22:45:33 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,16 @@
 int main(void)
 {
 	int a;
-	
+
 	for (a = 0; a <= 9; a++)
 	{
-		putchar('0' + a);
-		putchar(',');
-		putchar(' ');
+		if (a != 9)
+		{
+			putchar('0' + a);
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	putchar('\n');
 	return (0);
 }

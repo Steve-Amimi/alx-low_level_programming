1e2442eaeae5d00cff471a25f829ddb1941c1b11
Stephen Amimi <stephenamimi@gmail.com>
2023-06-25 17:08:34 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * This program prints of digits combo
+ *
+ * Return: (0) Success
+ **/
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			putchar('0' + a);
+			putchar('0' + b);
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

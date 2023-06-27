e7d51bd063595b3e408f3930a6aed50b22afa254
Stephen Amimi <stephenamimi@gmail.com>
2023-06-27 05:30:05 +0100

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - this program swaps int value
+ * @a: integer to swap
+ * @b: integer to wap
+ **/
+
+void swap_int(int *a, int *b)
+{
+	int a;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}

f11dec11dc7f1e4bfcf2d42ebdb16c4754a002f8
Stephen Amimi <stephenamimi@gmail.com>
2023-06-29 12:59:39 -0700

    new

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -9,8 +9,10 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-char *leet(char *a);
-char *rot13(char *a);
+char *leet(char *);
+char *rot13(char *);
 void print_number(int n);
-char *infinite_add(char *n1, char n*2, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+#endif

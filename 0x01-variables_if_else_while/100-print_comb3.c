dd424ea538e7623de55c036705b725fc1f820b3f
Stephen Amimi <stephenamimi@gmail.com>
2023-06-29 05:35:53 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,20 +9,19 @@
  **/
 int main(void)
 {
-        int a;
-        int b;
+	int a;
+	int b;
 
-        for (a = 0; a <= 9; a++)
-        {
-                for (b = 0; b <= 9; b++)
-                {
-                        putchar('0' + a);
-                        putchar('0' + b);
-                        putchar(',');
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			putchar('0' + a);
+			putchar('0' + b);
+			putchar(',');
 			putchar(' ');
-                }
-        }
-        putchar('\n');
-        return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
